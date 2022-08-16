565895717b2161e80b93423803ff4aa50c520fff
BLOODYKNF <93147411+BLOODYKNF@users.noreply.github.com>
2022-08-17 00:40:49 +0400

    Update ნომრების დაყრა.py

--- ნომრების დაყრა.py
+++ ნომრების დაყრა.py
@@ -1,26 +1,26 @@
-#This Moduls Is Edited By Bloodyofc(Bloody Knife), tg:@bloodyofc
 from .. import loader, utils
 import asyncio
 import requests
 import json
 
 from telethon.tl.types import *
+# requires: requests
 
 
 @loader.tds
 class BCheckMod(loader.Module):
-    """იუზერის გაჟონილი ნომრის პოვნა"""
+    """Bulk chat phone number leakage check"""
     strings = {
         "name": "BCheck",
-       'checking': '<b>მოწმდება ჩატი...</b>',
-       'check_in_progress': 'მიმდინარეობს შემოწმება...',
-       'search_header': "შედეგი: ",
-       'not_found': "შედეგი: <code>ვერ მოიძებნა</code>",
-       'check_started': 'დაიწყო ჩატის შემოწმება'
+       'checking': '<b>Checking chat...</b>',
+       'check_in_progress': 'Check in progress...',
+       'search_header': "Search result: ",
+       'not_found': "Result: <code>Not found</code>",
+       'check_started': 'Starting check in chat'
     }
 
     async def bcheckcmd(self, message):
-        """ყველა მემბერების შემოწმება"""
+        """Check all chat members for leaked numbers"""
         await utils.answer(message, self.strings('checking'))
 
         check_result = self.strings('search_header', message)
@@ -42,23 +42,24 @@ class BCheckMod(loader.Module):
         await message.edit(check_result)
 
     async def bchecksilentcmd(self, message):
-        """ჩუმი შემოწმების ფუნქცია"""
+        """Silent mode of bcheck"""
         await message.delete()
         msg = await message.client.send_message('me', self.strings('check_started', message))
         check_result = self.strings('search_header', message)
-
         async for user in message.client.iter_participants(message.to_id):
+            f = open("result.txt", "a", encoding="utf-8")
+            #f.write(str(user.id) + ':\n')
             dt = requests.get(
                 'http://api.murix.ru/eye?v=1.2&uid=' + str(user.id)).json()
             # await message.reply("<code>" + json.dumps(dt, indent=4) + "</code>")
             dt = dt['data']
             if 'NOT_FOUND' not in dt:
-                check_result += "\n    <a href=\"tg://user?id=" + str(user.id) + "}\">" + (str(
-                    user.first_name) + " " + str(user.last_name)).replace(' None', "") + "</a>: <code>" + dt + "</code>"
-                await msg.edit(check_result + '\n\n' + self.strings('check_in_progress', message))
+                # check_result += "\n    <a href=\"tg://user?id=" + str(user.id) + "}\">" + (str(
+                #     user.first_name) + " " + str(user.last_name)).replace(' None', "") + "</a>: \n" + "ID:\n" + str(user.id) + "Phone: <code>" + dt + "</code>"
+                f.write("First name: " + (str(user.first_name) + "\nLast name: " + str(user.last_name)).replace(' None', "") + "\nUsername: " + str(user.username) + "\nID: " + str(user.id) + "\nPhone: " + dt + "\n\n")
+                #await msg.edit(check_result + '\n\n' + self.strings('check_in_progress', message))
+            f.close()
             await asyncio.sleep(1)
-
-        if check_result == self.strings('search_header', message):
-            check_result = self.strings('not_found', message)
-
-        await msg.edit(check_result)
+        f = open("result.txt", "a")
+        f.write("Finished")
+        f.close()
