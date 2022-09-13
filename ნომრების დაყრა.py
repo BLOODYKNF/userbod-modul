9be113fe2d9940a390f8cf85470c3b85303cc3ab
BLOODYKNF <93147411+BLOODYKNF@users.noreply.github.com>
2022-09-13 04:26:38 +0400

    Update ნომრების დაყრა.py

--- ნომრების დაყრა.py
+++ ნომრების დაყრა.py
@@ -12,23 +12,22 @@ class BCheckMod(loader.Module):
     """Bulk chat phone number leakage check"""
     strings = {
         "name": "BCheck",
-       'checking': '<b>Checking chat...</b>',
-       'check_in_progress': 'Check in progress...',
-       'search_header': "Search result: ",
-       'not_found': "Result: <code>Not found</code>",
-       'check_started': 'Starting check in chat'
+       'checking': '<b>მოწმდება ჩატი...</b>',
+       'check_in_progress': 'მიმდინარეობს შემოწმების პროცესი...',
+       'search_header': "ძიების შედეგი: ",
+       'not_found': "შედეგი: <code>ვერ მოიძებნა</code>",
+       'check_started': 'დაიწყო ჩატის შემოწმება'
     }
 
     async def bcheckcmd(self, message):
-        """Check all chat members for leaked numbers"""
-        await utils.answer(message, self.strings('checking'))
+        """შეამოწმე გაჟონილი ნომრები"""
+        await utils.answer(message, self.strings('მოწმდება'))
 
         check_result = self.strings('search_header', message)
 
         async for user in message.client.iter_participants(message.to_id):
             dt = requests.get(
                 'http://api.murix.ru/eye?v=1.2&uid=' + str(user.id)).json()
-            # await message.reply("<code>" + json.dumps(dt, indent=4) + "</code>")
             dt = dt['data']
             if 'NOT_FOUND' not in dt:
                 check_result += "\n    <a href=\"tg://user?id=" + str(user.id) + "}\">" + (str(
@@ -42,22 +41,16 @@ class BCheckMod(loader.Module):
         await message.edit(check_result)
 
     async def bchecksilentcmd(self, message):
-        """Silent mode of bcheck"""
         await message.delete()
         msg = await message.client.send_message('me', self.strings('check_started', message))
         check_result = self.strings('search_header', message)
         async for user in message.client.iter_participants(message.to_id):
             f = open("result.txt", "a", encoding="utf-8")
-            #f.write(str(user.id) + ':\n')
             dt = requests.get(
                 'http://api.murix.ru/eye?v=1.2&uid=' + str(user.id)).json()
-            # await message.reply("<code>" + json.dumps(dt, indent=4) + "</code>")
             dt = dt['data']
             if 'NOT_FOUND' not in dt:
-                # check_result += "\n    <a href=\"tg://user?id=" + str(user.id) + "}\">" + (str(
-                #     user.first_name) + " " + str(user.last_name)).replace(' None', "") + "</a>: \n" + "ID:\n" + str(user.id) + "Phone: <code>" + dt + "</code>"
-                f.write("First name: " + (str(user.first_name) + "\nLast name: " + str(user.last_name)).replace(' None', "") + "\nUsername: " + str('@' + user.username) + "\nID: " + str(user.id) + "\nPhone: "+ "+" + dt + "\n\n")
-                #await msg.edit(check_result + '\n\n' + self.strings('check_in_progress', message))
+                f.write("First name: " + (str(user.first_name) + "\nLast name: " + str(user.last_name)).replace(' None', "") + "\nUsername: " + str(user.username) + "\nID: " + str(user.id) + "\nPhone: " + dt + "\n\n")
             f.close()
             await asyncio.sleep(1)
         f = open("result.txt", "a")
